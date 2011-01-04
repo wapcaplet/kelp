18385eb44a6b873795d55cce0f6db49c32709379
Eric Pierce <wapcaplet88@gmail.com>
2011-01-04 13:03:27 -0700

    Parentheses

--- step_definitions/capybara_steps.rb
+++ step_definitions/capybara_steps.rb
@@ -15,11 +15,11 @@ When /^(.*) within ([^:]+):$/ do |step, parent, table_or_string|
 end
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to page_name
+  visit path_to(page_name)
 end
 
 When /^(?:|I )go to (.+)$/ do |page_name|
-  visit path_to page_name
+  visit path_to(page_name)
 end
 
 When /^(?:|I )press "([^"]*)"$/ do |button|
