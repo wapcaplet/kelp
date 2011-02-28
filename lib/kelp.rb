fe04ed86a16e8229197ab9bf97c172eafd89919f
Eric Pierce <wapcaplet88@gmail.com>
2011-02-27 19:15:16 -0700

    Added Kelp.driver attribute (:capybara or :webrat)

--- lib/kelp.rb
+++ lib/kelp.rb
@@ -6,3 +6,21 @@ require 'kelp/navigation'
 require 'kelp/scoping'
 require 'kelp/visibility'
 
+module Kelp
+  class << self
+    attr_writer :driver
+
+    # @return [Symbol]
+    #   Name of the default driver used by Kelp
+    def default_driver
+      :capybara
+    end
+
+    # @return [Symbol]
+    #   Name of the current driver used by Kelp (:capybara or :webrat)
+    def driver
+      @driver || default_driver
+    end
+  end
+end
+
