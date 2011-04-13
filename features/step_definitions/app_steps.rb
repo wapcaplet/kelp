a29fcf3663f2260622a42f2a279de3876cacec32
Eric Pierce <wapcaplet88@gmail.com>
2011-04-12 18:18:09 -0600

    Moved step definitions into lib/generators
    In preparation for creating a Rails generator for step definitions, this
    commit moves the kelp_steps.rb (and still-unused-and-untested
    capybara_steps.rb) into lib/generators/kelp/step_definitions, and
    adjusts the cucumber self-tests to accommodate this. Also fixed a small
    typo in kelp_steps.rb.

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 Given /^a (\w+) application with the latest Kelp step definitions$/ do |platform|
   app_dir = File.join(root_dir, 'examples', "#{platform}_app")
   installed_steps = File.join(app_dir, 'features', 'step_definitions', 'kelp_steps.rb')
-  latest_steps = File.join(root_dir, 'step_definitions', 'kelp_steps.rb')
+  latest_steps = File.join(root_dir, 'lib', 'generators', 'kelp', 'step_definitions', 'kelp_steps.rb')
   # Remove the installed kelp_steps.rb, if any, then install the latest one
   FileUtils.rm(installed_steps) if File.exists?(installed_steps)
   FileUtils.cp_r(latest_steps, installed_steps)
