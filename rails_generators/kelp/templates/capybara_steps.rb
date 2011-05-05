e5cfb2695e740a4a56605e01f77c664917b6226b
Eric Pierce <wapcaplet88@gmail.com>
2011-05-05 12:56:27 -0600

    Move most stepdefs into kelp_steps, refactor
    This commit moves pretty far toward replacing all of cucumber-rails'
    step definitions with kelp equivalents. Also refactored several stepdefs
    into separate "should" and "should not" versions, so they can be
    one-liners.

--- rails_generators/kelp/templates/capybara_steps.rb
+++ rails_generators/kelp/templates/capybara_steps.rb
@@ -14,95 +14,9 @@ When /^(.*) within ([^:]+):$/ do |step, parent, table_or_string|
   scope_within(parent) { When "#{step}:", table_or_string }
 end
 
-Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-
-When /^(?:|I )go to (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-
-When /^(?:|I )press "([^"]*)"$/ do |button|
-  click_button button
-end
-
-When /^(?:|I )follow "([^"]*)"$/ do |link|
-  click_link link
-end
-
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-  fill_in field, :with => value
-end
-
-When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
-  fill_in field, :with => value
-end
-
-# Use this to fill in an entire form with data from a table. Example:
-#
-#   When I fill in the following:
-#     | Account Number | 5002       |
-#     | Expiry date    | 2009-11-01 |
-#     | Note           | Nice guy   |
-#     | Wants Email?   |            |
-#
-# TODO: Add support for checkbox, select og option
-# based on naming conventions.
-#
-When /^(?:|I )fill in the following:$/ do |fields|
-  fill_in_fields fields.rows_hash
-end
-
-When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
-  select value, :from => field
-end
-
-When /^(?:|I )check "([^"]*)"$/ do |field|
-  check field
-end
-
-When /^(?:|I )uncheck "([^"]*)"$/ do |field|
-  uncheck field
-end
-
-When /^(?:|I )choose "([^"]*)"$/ do |field|
-  choose field
-end
 
 When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
   attach_file field, File.expand_path(path)
-end
-
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
-  should_see text
-end
-
-Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
-  should_see Regexp.new(regexp)
-end
-
-Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  should_not_see text
-end
-
-Then /^(?:|I )should not see \/([^\/]*)\/$/ do |regexp|
-  should_not_see Regexp.new(regexp)
-end
-
-Then /^the "([^"]*)" field(?: within (.*))? should contain "([^"]*)"$/ do |field, parent, value|
-  field_should_contain field, value, :within => parent
-end
-
-Then /^the "([^"]*)" field(?: within (.*))? should not contain "([^"]*)"$/ do |field, parent, value|
-  field_should_not_contain field, value, :within => parent
-end
-
-Then /^the "([^"]*)" checkbox(?: within (.*))? should be checked$/ do |label, parent|
-  checkbox_should_be_checked label, :within => parent
-end
-
-Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label, parent|
-  checkbox_should_not_be_checked label, :within => parent
 end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
--- rails_generators/kelp/templates/kelp_steps.rb
+++ rails_generators/kelp/templates/kelp_steps.rb
@@ -35,10 +35,83 @@ module KelpStepHelper
 end
 World(KelpStepHelper)
 
-SHOULD_OR_NOT = /(should|should not)/
-WITHIN = /(?: within "([^\"]+)")?/
+
+# Patterns shared by step definitions
+I = /(?:|I )/
+TEXT = /"([^\"]+)"/
+REGEX = /\/([^\/]*)\//
+WITHIN = /(?: within (.+))?/
 ELEMENT = /(?:field|checkbox|dropdown|button)/
-STR = /([^\"]+)/
+
+
+# ==========================
+# NAVIGATION
+# ==========================
+
+Given /^#{I}(am on|go to) (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+
+# Press a button on a webpage.
+When /^#{I}press #{TEXT}#{WITHIN}$/ do |button, selector|
+  press(button, :within => selector)
+end
+
+
+# CLick a link in a webpage.
+When /^#{I}follow #{TEXT}#{WITHIN}$/ do |link, selector|
+  follow(link, :within => selector)
+end
+
+
+# Click a link in a table row that contains the given text.
+# This can be used to click the "Edit" link for a specific record.
+#
+# Examples:
+#
+#   When I follow "Edit" next to "John"
+#
+When /^#{I}follow #{TEXT} next to #{TEXT}$/ do |link, next_to|
+  click_link_in_row(link, next_to)
+end
+
+
+# ==========================
+# VISIBILITY
+# ==========================
+
+
+# Verify that the given string is visible on the webpage.
+#
+# Examples:
+#
+#   Then I should see "Favorite color"
+#   But I should not see "Least favorite color"
+#
+Then /^#{I}should see #{TEXT}#{WITHIN}$/ do |text, selector|
+  should_see(text, :within => selector)
+end
+
+Then /^#{I}should not see #{TEXT}#{WITHIN}$/ do |text, selector|
+  should_not_see(text, :within => selector)
+end
+
+
+# Verify that the given regular expression matches some part of the webpage.
+#
+# Examples:
+#
+#   Then I should see /Favorite (color|food)/
+#
+Then /^#{I}should see #{REGEX}#{WITHIN}$/ do |regexp, selector|
+  should_see(Regexp.new(regexp), :within => selector)
+end
+
+Then /^#{I}should not see #{REGEX}#{WITHIN}$/ do |regexp, selector|
+  should_not_see(Regexp.new(regexp), :within => selector)
+end
+
 
 # Verify the presence or absence of multiple text strings in the page,
 # or within a given context.
@@ -60,13 +133,12 @@ STR = /([^\"]+)/
 #     Hash Browns
 #     """
 #
-Then /^I #{SHOULD_OR_NOT} see the following#{WITHIN}:$/ do |expect, selector, items|
-  strings = listify(items)
-  if expect == 'should'
-    should_see strings, :within => selector
-  else
-    should_not_see strings, :within => selector
-  end
+Then /^#{I}should see(?: the following)?#{WITHIN}:$/ do |selector, items|
+  should_see listify(items), :within => selector
+end
+
+Then /^#{I}should not see(?: the following)?#{WITHIN}:$/ do |selector, items|
+  should_not_see listify(items), :within => selector
 end
 
 
@@ -82,95 +154,16 @@ end
 #   And I should not see a table row containing:
 #     | Eric | Delete |
 #
-Then /^I #{SHOULD_OR_NOT} see (?:a table row|table rows)#{WITHIN} containing:$/ do |expect, selector, rows|
+Then /^#{I}should see (?:a table row|table rows)#{WITHIN} containing:$/ do |selector, rows|
   rows.raw.each do |fields|
-    if expect == 'should'
-      should_see_in_same_row(fields, :within => selector)
-    else
-      should_not_see_in_same_row(fields, :within => selector)
-    end
+    should_see_in_same_row(fields, :within => selector)
   end
 end
 
-
-# Verify that a dropdown has a given value selected. This verifies the visible
-# value shown to the user, rather than the value attribute of the selected
-# option element.
-#
-# Examples:
-#
-#   Then the "Height" dropdown should equal "Average"
-#
-Then /^the "#{STR}" dropdown#{WITHIN} should equal "#{STR}"$/ do |dropdown, selector, value|
-  dropdown_should_equal(dropdown, value, :within => selector)
-end
-
-
-# Verify that a dropdown includes or doesn't include the given value.
-#
-# Examples:
-#
-#   Then the "Height" dropdown should include "Tall"
-#
-Then /^the "#{STR}" dropdown#{WITHIN} #{SHOULD_OR_NOT} include "#{STR}"$/ do |dropdown, selector, expect, value|
-  if expect == 'should'
-    dropdown_should_include(dropdown, value, :within => selector)
-  else
-    dropdown_should_not_include(dropdown, value, :within => selector)
+Then /^#{I}should not see (?:a table row|table rows)#{WITHIN} containing:$/ do |selector, rows|
+  rows.raw.each do |fields|
+    should_not_see_in_same_row(fields, :within => selector)
   end
-end
-
-
-# Verify that a dropdown includes or doesn't include all values in the given
-# table or multiline string.
-#
-# Examples:
-#
-#   Then the "Height" dropdown should include:
-#     | Short   |
-#     | Average |
-#     | Tall    |
-#
-#   Then the "Favorite Colors" dropdown should include:
-#     """
-#     Red
-#     Green
-#     Blue
-#     """
-#
-Then /^the "#{STR}" dropdown#{WITHIN} #{SHOULD_OR_NOT} include:$/ do |dropdown, selector, expect, values|
-  listify(values).each do |value|
-    if expect == 'should'
-      dropdown_should_include(dropdown, value, :within => selector)
-    else
-      dropdown_should_not_include(dropdown, value, :within => selector)
-    end
-  end
-end
-
-
-# Verify that a given field is empty or nil.
-#
-# Examples:
-#
-#   Then the "First name" field should be empty
-#
-Then /^the "#{STR}" field#{WITHIN} should be empty$/ do |field, selector|
-  field_should_be_empty(field, :within => selector)
-end
-
-
-# Verify multiple fields in a form, optionally restricted to a given selector.
-# Fields may be text inputs or dropdowns.
-#
-# Examples:
-#
-#   Then the fields should contain:
-#     | First name | Eric   |
-#     | Last name  | Pierce |
-#
-Then /^the fields#{WITHIN} should contain:$/ do |selector, fields|
-  fields_should_contain_within(selector, fields.rows_hash)
 end
 
 
@@ -183,12 +176,12 @@ end
 #   Then I should see "Edit" next to "John"
 #   And I should not see "Delete" next to "John"
 #
-Then /^I #{SHOULD_OR_NOT} see "#{STR}" next to "#{STR}"#{WITHIN}$/ do |expect, text, next_to, selector|
-  if expect == 'should'
-    should_see_in_same_row([text, next_to], :within => selector)
-  else
-    should_not_see_in_same_row([text, next_to], :within => selector)
-  end
+Then /^#{I}should see #{TEXT} next to #{TEXT}#{WITHIN}$/ do |text, next_to, selector|
+  should_see_in_same_row([text, next_to], :within => selector)
+end
+
+Then /^#{I}should not see #{TEXT} next to #{TEXT}#{WITHIN}$/ do |text, next_to, selector|
+  should_not_see_in_same_row([text, next_to], :within => selector)
 end
 
 
@@ -210,26 +203,200 @@ end
 #     Delete
 #     """
 #
-Then /^I #{SHOULD_OR_NOT} see the following next to "#{STR}"#{WITHIN}:$/ do |expect, next_to, selector, items|
-  listify(items).each do |text|
-    if expect == 'should'
-      should_see_in_same_row([text, next_to], :within => selector)
-    else
-      should_not_see_in_same_row([text, next_to], :within => selector)
-    end
-  end
+Then /^#{I}should see the following next to #{TEXT}#{WITHIN}:$/ do |next_to, selector, items|
+  should_see_in_same_row(listify(items) + [next_to], :within => selector)
+end
+
+Then /^#{I}should not see the following next to #{TEXT}#{WITHIN}:$/ do |next_to, selector, items|
+  should_not_see_in_same_row(listify(items) + [next_to], :within => selector)
 end
 
 
-# Click a link in a table row that contains the given text.
-# This can be used to click the "Edit" link for a specific record.
+# ==========================
+# FORMS
+# ==========================
+
+
+# Fill in a form field with a given value.
 #
 # Examples:
 #
-#   When I follow "Edit" next to "John"
+#   When I fill in "Favorite color" with "Green"
 #
-When /^I follow "#{STR}" next to "#{STR}"$/ do |link, next_to|
-  click_link_in_row(link, next_to)
+When /^#{I}fill in #{TEXT} with #{TEXT}#{WITHIN}$/ do |field, value, selector|
+  fill_in_field(field, value, :within => selector)
+end
+
+
+# Fill in a form field with a given value.
+#
+# Examples:
+#
+#   When I fill in "Green" for "Favorite color"
+#
+When /^#{I}fill in #{TEXT} for #{TEXT}#{WITHIN}$/ do |value, field, selector|
+  fill_in_field(field, value, :within => selector)
+end
+
+
+# Fill in multiple form fields from values in a table.
+# The fields may be textboxes, dropdowns/listboxes, or checkboxes.
+#
+# Examples:
+#
+#   When I fill in the following:
+#     | First name  | Alton   |
+#     | Last name   | Brown   |
+#     | Hair        | blonde  |
+#     | Has TV show | checked |
+#
+When /^#{I}fill in the following#{WITHIN}:$/ do |fields, selector|
+  fill_in_fields(fields.rows_hash, :within => selector)
+end
+
+
+# Select a value from a dropdown.
+When /^#{I}select #{TEXT} from #{TEXT}$/ do |value, field|
+  select value, :from => field
+end
+
+
+# Check a checkbox.
+When /^#{I}check #{TEXT}$/ do |field|
+  check field
+end
+
+
+# Uncheck a checkbox.
+When /^#{I}uncheck #{TEXT}$/ do |field|
+  uncheck field
+end
+
+
+# Choose a given radio button.
+When /^#{I}choose #{TEXT}$/ do |field|
+  choose field
+end
+
+
+# Verify that a given field is empty or nil.
+#
+# Examples:
+#
+#   Then the "First name" field should be empty
+#
+Then /^the #{TEXT} field#{WITHIN} should be empty$/ do |field, selector|
+  field_should_be_empty(field, :within => selector)
+end
+
+
+# Verify that a text field in a form does or doesn't contain a value.
+#
+# Examples:
+#
+#   Then the "Last name" field should contain "Brown"
+#   But the "First name" field should not contain "Leroy"
+#
+Then /^the #{TEXT} field#{WITHIN} should contain #{TEXT}$/ do |field, selector, value|
+  field_should_contain(field, value, :within => selector)
+end
+
+Then /^the #{TEXT} field#{WITHIN} should not contain #{TEXT}$/ do |field, selector, value|
+  field_should_not_contain(field, value, :within => selector)
+end
+
+
+# Verify that a dropdown has a given value selected. This verifies the visible
+# value shown to the user, rather than the value attribute of the selected
+# option element.
+#
+# Examples:
+#
+#   Then the "Height" dropdown should equal "Average"
+#
+Then /^the #{TEXT} dropdown#{WITHIN} should equal #{TEXT}$/ do |dropdown, selector, value|
+  dropdown_should_equal(dropdown, value, :within => selector)
+end
+
+
+# Verify that a dropdown includes or doesn't include the given value.
+#
+# Examples:
+#
+#   Then the "Height" dropdown should include "Tall"
+#
+Then /^the #{TEXT} dropdown#{WITHIN} should include #{TEXT}$/ do |dropdown, selector, value|
+  dropdown_should_include(dropdown, value, :within => selector)
+end
+
+Then /^the #{TEXT} dropdown#{WITHIN} should not include #{TEXT}$/ do |dropdown, selector, value|
+  dropdown_should_not_include(dropdown, value, :within => selector)
+end
+
+
+# Verify that a dropdown includes or doesn't include all values in the given
+# table or multiline string.
+#
+# Examples:
+#
+#   Then the "Height" dropdown should include:
+#     | Short   |
+#     | Average |
+#     | Tall    |
+#
+#   Then the "Favorite Colors" dropdown should include:
+#     """
+#     Red
+#     Green
+#     Blue
+#     """
+#
+Then /^the #{TEXT} dropdown#{WITHIN} should include:$/ do |dropdown, selector, values|
+  dropdown_should_include(dropdown, listify(values), :within => selector)
+end
+
+Then /^the #{TEXT} dropdown#{WITHIN} should not include:$/ do |dropdown, selector, values|
+  dropdown_should_not_include(dropdown, listify(values), :within => selector)
+end
+
+
+# Verify multiple fields in a form, optionally restricted to a given selector.
+# Fields may be text inputs or dropdowns.
+#
+# Examples:
+#
+#   Then the fields should contain:
+#     | First name | Eric   |
+#     | Last name  | Pierce |
+#
+Then /^the fields#{WITHIN} should contain:$/ do |selector, fields|
+  fields_should_contain_within(selector, fields.rows_hash)
+end
+
+
+# Verify that a checkbox is checked or unchecked.
+# "should not be checked" and "should be unchecked" are equivalent, and
+# "should be checked" and "should not be unchecked" are equivalent.
+#
+# Examples:
+#
+#   Then the "I like apples" checkbox should be checked
+#   And the "I like celery" checkbox should not be checked
+#
+Then /^the #{TEXT} checkbox#{WITHIN} should be (checked|unchecked)$/ do |checkbox, selector, state|
+  if state == 'checked'
+    checkbox_should_be_checked(checkbox, :within => selector)
+  else
+    checkbox_should_not_be_checked(checkbox, :within => selector)
+  end
+end
+
+Then /^the #{TEXT} checkbox#{WITHIN} should not be (checked|unchecked)$/ do |checkbox, selector, state|
+  if state == 'unchecked'
+    checkbox_should_be_checked(checkbox, :within => selector)
+  else
+    checkbox_should_not_be_checked(checkbox, :within => selector)
+  end
 end
 
 
@@ -242,10 +409,21 @@ end
 #   Then the "Like" checkbox next to "Apple" should be checked
 #   And the "Like" checkbox next to "Banana" should be unchecked
 #
-Then /^the "#{STR}" checkbox next to "#{STR}"#{WITHIN} #{SHOULD_OR_NOT} be (checked|unchecked)$/ do |checkbox, next_to, selector, expect, state|
+Then /^the #{TEXT} checkbox next to #{TEXT}#{WITHIN} should be (checked|unchecked)$/ do |checkbox, next_to, selector, state|
 
   within(:xpath, xpath_row_containing(next_to)) do
-    if (expect == 'should' && state == 'checked') || (expect == 'should not' && state == 'unchecked')
+    if state == 'checked'
+      checkbox_should_be_checked(checkbox, :within => selector)
+    else
+      checkbox_should_not_be_checked(checkbox, :within => selector)
+    end
+  end
+end
+
+Then /^the #{TEXT} checkbox next to #{TEXT}#{WITHIN} should not be (checked|unchecked)$/ do |checkbox, next_to, selector, state|
+
+  within(:xpath, xpath_row_containing(next_to)) do
+    if state == 'unchecked'
       checkbox_should_be_checked(checkbox, :within => selector)
     else
       checkbox_should_not_be_checked(checkbox, :within => selector)
